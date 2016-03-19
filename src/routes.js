b55fd88c51bc3beabc16ef90051af1a8dae410a8
DanSnow <dododavid006@gmail.com>
2016-03-20 00:46:37 +0800

    Update route

--- src/routes.js
+++ src/routes.js
@@ -3,11 +3,12 @@ import { Route, IndexRoute, Redirect } from 'react-router';
 
 import CoreLayout from './components/CoreLayout';
 import App from './components/App';
-import RecordDetail from './components/RecordDetail';
+import RecordList, { RecordsQuery } from './components/RecordList';
+import RecordDetail, { RecordQuery } from './components/RecordDetail';
 
 export default (
   <Route path='/' component={ CoreLayout }>
-    <IndexRoute component={ App } />
-    <Route path=':id' component={ RecordDetail } />
+    <IndexRoute name="list" component={ RecordList } queries={ RecordsQuery } />
+    <Route name="detail" path=':id' component={ RecordDetail } queries={ RecordQuery } />
   </Route>
 );
