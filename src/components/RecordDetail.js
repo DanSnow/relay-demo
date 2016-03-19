47448597bd4fca28f1cf7fafec6e6f6e7ffac08d
DanSnow <dododavid006@gmail.com>
2016-03-20 00:46:28 +0800

    Finish RecordDetail

--- src/components/RecordDetail.js
+++ src/components/RecordDetail.js
@@ -1,13 +1,45 @@
 import React, { Component, PropTypes } from 'react';
+import Relay from 'react-relay';
+import { Link } from 'react-router';
 
-export default class RecordDetail extends Component {
+class RecordDetail extends Component {
   render() {
+    let { record } = this.props;
     return (
-      <h3>detail for: {this.props.params.id}</h3>
+      <div>
+        <h3>Detail for: { record.name }</h3>
+        <ul>
+          <li> Phone: { record.phone } </li>
+        </ul>
+        <Link to="/"> Back to home </Link>
+      </div>
     );
   }
 }
 
 RecordDetail.propTypes = {
-  params: PropTypes.object.isRequired
+  record: PropTypes.object.isRequired
 };
+
+RecordDetail = Relay.createContainer(RecordDetail, {
+  fragments: {
+    record: () => Relay.QL`
+      fragment on Record {
+        name,
+        phone
+      }
+    `
+  }
+});
+
+export default RecordDetail;
+
+export const RecordQuery = {
+  record: (Component) => Relay.QL`
+    query {
+      record(id: $id) {
+        ${Component.getFragment('record')}
+      }
+    }
+  `
+}
