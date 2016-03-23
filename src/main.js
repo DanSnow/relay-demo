3f2e7e35d521b1462ba4671886b61b9178bc2b7a
DanSnow <dododavid006@gmail.com>
2016-03-23 21:52:04 +0800

    Inject tap event

--- src/main.js
+++ src/main.js
@@ -4,10 +4,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { createHistory } from 'history';
 import { Router, useRouterHistory } from 'react-router';
+import injectTapEventPlugin from 'react-tap-event-plugin';
 import routes from './routes';
 import createRelayContainer from './create-relay-container';
 
 const history = useRouterHistory(createHistory)();
+
+injectTapEventPlugin();
 
 ReactDOM.render((
   <Router history={ history } createElement={ createRelayContainer }>
