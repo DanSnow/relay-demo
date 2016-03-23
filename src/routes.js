26a2129040350f1ae9ce3b6c95c9f486e71b830a
DanSnow <dododavid006@gmail.com>
2016-03-23 21:52:30 +0800

    Fix route

--- src/routes.js
+++ src/routes.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { Route, IndexRoute, Redirect } from 'react-router';
 
 import CoreLayout from './components/CoreLayout';
-import App from './components/App';
 import RecordList, { RecordsQuery } from './components/RecordList';
 import RecordDetail, { RecordQuery } from './components/RecordDetail';
 
